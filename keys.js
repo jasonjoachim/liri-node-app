df456a5f29628774e509547eb685d5635b9a17e3
jasonjoachim <joachim.jason@gmail.com>
2017-09-06 10:03:47 -0500

    basic setup w keys

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<ZpKXiRDIJAcyzQYiMwVQ52UIc>',
+  consumer_secret: '<zPrBJDFIrlgMjMl2nYJNzZ3nX3BjQzE5Dsl3BqpqGiNUJxwIJu>',
+  access_token_key: '<905158979621412864-P1WjbMSRlvdxSNaz5VPfetKd2N9f26z>',
+  access_token_secret: '<A7bHUbryCfaXLjRRmKLg5Fuob5bQqzFDpYPzKzQXN9Xiq>',
+}
